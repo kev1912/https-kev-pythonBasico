d3cb541af1675caf15be1ae4a09980743bf5e68c
kev1912 <138842241+kev1912@users.noreply.github.com>
2023-07-07 23:31:05 -0600

    Add files via upload

---
+++ python nivel 2/main.py
@@ -0,0 +1,39 @@
+from trig import *
+import math
+import datetime
+#obtener fecha y hora actuales en el sistema
+ahora = datetime.datetime.now()
+
+seno = 0
+coseno = 0
+tangente = 0
+
+#calcular seno
+seno = math.sin(90)
+#calcular coseno
+coseno = math.cos(90)
+#calcular tangente
+tangente = math.tan(90)
+
+
+
+print("el seno de 90 radianes es:", seno)
+print("el coseno de 90 radianes es:", coseno)
+print("la tangente de 90 radianes es:", tangente)
+print("")
+
+print(ahora)
+print(type(ahora))
+print(ahora.strftime('%d/%m/%y %H:%M:%S'))
+
+
+
+
+    
+
+    
+    
+
+
+
+
---
+++ python nivel 2/trig.py
@@ -0,0 +1,13 @@
+class trig():
+
+    def __init__(self):
+        self.pi = math.pi
+
+    def seno(self):
+        return math.sin(self.pi)
+
+    def coseno(self):
+        return math.cos(self.pi)
+
+    def tangente(self):
+        return math.tan(self.pi)
